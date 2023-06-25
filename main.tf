678d9c4439ed6e29dd552d08eb1aa6dec852d210
Gerry Wolfe <crywolfe@users.noreply.github.com>
2023-06-25 14:51:44 -0500

    add underscore

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc.id
+  vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
