9832171fcc90499e47e4c6dd777488d4161a7bf4
Gerry Wolfe <crywolfe@users.noreply.github.com>
2023-06-25 14:36:52 -0500

    fix error

--- main.tf
+++ main.tf
@@ -17,10 +17,10 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-1b", "us-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
